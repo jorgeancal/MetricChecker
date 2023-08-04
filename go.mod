e2938a5379a2ad53cc36b2be7062be7b37451e4b
jorgeancal <jorgeancal@gmail.com>
2023-08-04 13:33:04 +0100

    feat: add metrics checker script

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/jorgeancal/metricChecker
+
+go 1.20
+
+require github.com/DataDog/datadog-api-client-go/v2 v2.15.0
+
+require (
+	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ metricChecker.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
+	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	metric := flag.String("metric", "", "The metric to search for in monitors")
+	flag.Parse()
+
+	if *metric == "" {
+		fmt.Println("Usage: go run main.go -metric=<metric>")
+		return
+	}
+
+	fmt.Println("Retrieving Monitors")
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+	api := datadogV1.NewMonitorsApi(apiClient)
+	resp, r, err := api.ListMonitors(ctx, *datadogV1.NewListMonitorsOptionalParameters())
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `MonitorsApi.ListMonitors`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	} else {
+		fmt.Fprintf(os.Stdout, "Searching metric in %d Monitors: %s\n", len(resp), *metric)
+	}
+
+	for _, element := range resp {
+		name := element.Name
+		query := element.Query
+		if strings.Contains(query, *metric) {
+			fmt.Fprintf(os.Stderr, "%s is being use in %s \n", *metric, *name)
+		}
+	}
+	fmt.Println("Retrieving Dashboards")
+
+	apidash := datadogV1.NewDashboardsApi(apiClient)
+	resp2, r, err := apidash.ListDashboards(ctx, *datadogV1.NewListDashboardsOptionalParameters().WithFilterShared(false))
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DashboardsApi.ListDashboards`: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+	} else {
+		fmt.Fprintf(os.Stdout, "Searching metric in %d Dashboards: %s\n", len(resp2.GetDashboards()), *metric)
+	}
+
+	for _, element := range resp2.GetDashboards() {
+		resp3, _, _ := apidash.GetDashboard(ctx, *element.Id)
+		responseContent, _ := json.MarshalIndent(resp3, "", "")
+		if strings.Contains(string(responseContent), *metric) {
+			fmt.Fprintf(os.Stderr, "%s is being use in %s \n", *metric, *element.Title)
+		}
+	}
+}
